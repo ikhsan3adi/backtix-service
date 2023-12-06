60332d51a94c798b186b34b5d4ae48e11f042881
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-12-06 15:59:36 +0700

    chore: Add needed dependencies & configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('BackTix API')
+    .setDescription('The BackTix API docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
 }
 bootstrap()
