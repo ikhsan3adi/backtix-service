c9aa11ad94534c749839281996e93da6e64e3bd5
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-12-08 16:05:37 +0700

    Update public.decorator.ts

--- src/auth/decorators/public.decorator.ts
+++ src/auth/decorators/public.decorator.ts
@@ -2,5 +2,7 @@ import { SetMetadata } from '@nestjs/common'
 
 export const IS_PUBLIC_KEY = 'isPublic'
 
-/// skip auth
+/**
+ * skip authentication
+ */
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true)
