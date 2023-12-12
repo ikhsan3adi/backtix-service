bba4ea58a8259dc5ea8149549bd95d2fef662e82
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-12-12 11:28:39 +0700

    fix: File stream error handling

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { FileController } from './file.controller'
 import { FileService } from './file.service'
+import { StorageModule } from '../storage/storage.module'
 
 @Module({
+  imports: [StorageModule],
   controllers: [FileController],
   providers: [FileService],
   exports: [FileService],
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,12 +1,23 @@
-import { Injectable, StreamableFile } from '@nestjs/common'
+import { Injectable, NotFoundException, StreamableFile } from '@nestjs/common'
 import { createReadStream } from 'fs'
 import { join } from 'path'
 import { config } from '../common/config'
+import { StorageService } from '../storage/storage.service'
 
 @Injectable()
 export class FileService {
+  constructor(private storageService: StorageService) {}
+
   getUploadedFile(path: string): StreamableFile {
+    this.checkFileExists(join(config.storage.uploadsPath, path))
+
     const file = createReadStream(join(config.storage.uploadsPath, path))
     return new StreamableFile(file)
+  }
+
+  checkFileExists(path: string) {
+    const fileExist = this.storageService.checkIfFileOrDirectoryExists(path)
+
+    if (!fileExist) throw new NotFoundException('File was not found')
   }
 }
