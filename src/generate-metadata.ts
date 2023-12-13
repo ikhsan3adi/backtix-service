a02051b6acd7f27af31b65b1f5f5e9b7935588dc
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-12-13 11:39:36 +0700

    refactor

--- src/generate-metadata.ts
+++ src/generate-metadata.ts
@@ -7,6 +7,6 @@ generator.generate({
     new ReadonlyVisitor({ introspectComments: true, pathToSource: __dirname }),
   ],
   outputDir: __dirname,
-  watch: true,
+  // watch: true,
   tsconfigPath: 'tsconfig.json',
 })
