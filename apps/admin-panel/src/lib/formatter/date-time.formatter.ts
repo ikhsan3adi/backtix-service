bb12ec5995df96a16b1e747371eacfb7f68407a1
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-12-28 15:18:07 +0700

    Create date-time.formatter.ts

---
+++ apps/admin-panel/src/lib/formatter/date-time.formatter.ts
@@ -0,0 +1,23 @@
+/**  Automatically convert from UTC to Local (for me it's +7 [Jakarta]) */
+const defaultDateTimeFormatter = new Intl.DateTimeFormat('id', {
+	day: '2-digit',
+	month: '2-digit',
+	year: 'numeric',
+	hour: '2-digit',
+	minute: '2-digit',
+	second: '2-digit',
+	timeZone: 'Asia/Jakarta',
+	hour12: false
+})
+
+const dateTimeFormatterWithoutSeconds = new Intl.DateTimeFormat('id', {
+	day: '2-digit',
+	month: '2-digit',
+	year: 'numeric',
+	hour: '2-digit',
+	minute: '2-digit',
+	timeZone: 'Asia/Jakarta',
+	hour12: false
+})
+
+export { dateTimeFormatterWithoutSeconds, defaultDateTimeFormatter }
