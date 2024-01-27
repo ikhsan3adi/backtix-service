8fec9af0a8704e250066bd66d14015b446f4d9e3
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-01-27 09:10:00 +0700

    Update app.controller.ts

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { Public } from './auth/decorators/public.decorator'
 
 @Controller()
+@Public()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
