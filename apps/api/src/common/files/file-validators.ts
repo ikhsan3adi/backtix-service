b9a1a86089fe9073d63d4cb6419ffb16933c35b6
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-02-24 14:28:17 +0700

    Update file-validators.ts
    Update filetype regex

--- apps/api/src/common/files/file-validators.ts
+++ apps/api/src/common/files/file-validators.ts
@@ -1,7 +1,7 @@
-import { CustomMaxFileSizeValidator } from '../validators/custom-max-file-size.validator'
 import { CustomFileTypeValidator } from '../validators/custom-file-type.validator'
+import { CustomMaxFileSizeValidator } from '../validators/custom-max-file-size.validator'
 
 export const imageValidators = [
   new CustomMaxFileSizeValidator({ maxSize: 5000000 }),
-  new CustomFileTypeValidator({ fileType: '.(png|jpeg|jpg|webp)' }),
+  new CustomFileTypeValidator({ fileType: /^.*(png|jpeg|jpg|webp)$/gi }),
 ]
